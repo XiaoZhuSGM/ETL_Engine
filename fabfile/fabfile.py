010f6b7105a3e0c9c569519943781aa946ea03ba
MisLink <gjq.uoiai@outlook.com>
2018-07-24 10:55:04 +0800

    build: Add fab deploy command for deployment.

--- fabfile/fabfile.py
+++ fabfile/fabfile.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from fabric import task
+
+
+@task
+def deploy(c):
+        with c.cd("/data/code/etl-engine/source"):
+            c.run("git checkout -- .")
+            c.run("git pull -r")
+        c.run("supervisorctl -c /data/code/supervisord.conf restart etl-engine")
