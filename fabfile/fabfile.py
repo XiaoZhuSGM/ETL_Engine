693e239dfe03dd888cdcdd1be33ddde5f1664341
MisLink <gjq.uoiai@outlook.com>
2018-07-24 11:33:03 +0800

    chore: Add deploy parameters.

--- fabfile/fabfile.py
+++ fabfile/fabfile.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
-from fabric import task
+from fabric import task, Connection
+
+ENV = {"dev": "172.31.16.17"}
 
 
 @task
-def deploy(c):
+def deploy(c, user, env="dev"):
+    """Deploy in <env>ironment by <user>.
+    """
+
+    with Connection(host=ENV[env], user=user) as c:
         with c.cd("/data/code/etl-engine/source"):
             c.run("git checkout -- .")
             c.run("git pull -r")
