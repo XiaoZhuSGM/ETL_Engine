7f716d8ed6f723de473abccc3f6f1bab0a4f13a4
郭嘉琪 <guojiaqi@chaomengdata.com>
2018-09-18 14:42:22 +0800

    modify ci.
    Signed-off-by: 郭嘉琪 <guojiaqi@chaomengdata.com>

--- fabfile/fabfile.py
+++ fabfile/fabfile.py
@@ -3,39 +3,22 @@ from fabric import task, Connection
 
 ENV = {"dev": "172.31.16.17"}
 
-
-@task
-def reload(c, env="dev"):
-    """Reload app."""
-    with Connection(host=ENV[env], user="centos") as c:
-        with c.cd("/data/code/etl-engine"):
-            result = c.run("kill -HUP $(cat gunicorn.pid)")
-            if result.failed:
-                with c.cd("/data/code"):
-                    c.run(f"supervisorctl -c supervisord.conf restart etl-engine")
+CODE_HOME = "/data/app/etl-engine"
 
 
 @task
 def supervisor(c, env="dev", command=""):
     """Supervisor command."""
     with Connection(host=ENV[env], user="centos") as c:
-        with c.cd("/data/code"):
-            c.run(f"supervisorctl -c supervisord.conf {command} etl-engine")
+        c.run(f"supervisorctl {command}")
 
 
 @task
-def restart_queue(c, env="dev"):
-    with Connection(host=ENV[env], user="centos") as c:
-        with c.cd("/data/code"):
-            c.run(f"supervisorctl -c supervisord.conf restart etl-engine:task-queue")
-
-
-@task(post=[reload, restart_queue])
 def deploy(c, env="dev", branch="dev"):
     """Deploy <branch> with <env>.
     """
     with Connection(host=ENV[env], user="centos") as c:
-        with c.cd("/data/code/etl-engine/source"):
+        with c.cd(CODE_HOME):
             # 1. 丢弃远端的修改
             c.run("git checkout -- .")
             # 2. 切换到正确的分支并拉取代码
@@ -45,6 +28,7 @@ def deploy(c, env="dev", branch="dev"):
             c.run(
                 "venv/bin/pip install -r requirements.txt -i https://pypi.doubanio.com/simple/"
             )
+        c.run("supervisorctl signal HUP etl-egnine:")
 
 
 @task
@@ -52,7 +36,7 @@ def manage(c, env="dev", command=""):
     """Use manage command in local.
     """
     with Connection(host=ENV[env], user="centos") as c:
-        with c.cd("/data/code/etl-engine"):
+        with c.cd(CODE_HOME):
             c.run(
                 f"ETL_ENVIREMENT={env} ./source/venv/bin/python source/manage.py {command}",
                 pty=True,
