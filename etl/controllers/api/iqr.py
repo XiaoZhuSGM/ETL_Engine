1e49ffc5938da91013dd18247e112c2c07e0f257
范佳楠 <beanan@192.168.1.57>
2018-10-24 13:43:10 +0800

    打印错误log->iqr

--- etl/controllers/api/iqr.py
+++ etl/controllers/api/iqr.py
@@ -10,6 +10,6 @@ def calculate_iqr(source_id):
         result = iqr_service.pipeline()
     except Exception as e:
         print(str(e))
-        return jsonify_with_error(APIError.SERVER_ERROR)
+        return jsonify_with_error(APIError.SERVER_ERROR, reason=str(e))
     else:
         return jsonify_with_data(APIError.OK, data=result)
