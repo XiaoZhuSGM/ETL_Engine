19795438a00654c76310e9eb0e75253ea83d40f3
郭嘉琪 <guojiaqi@chaomengdata.com>
2018-10-29 14:35:13 +0800

    默认显示60天.
    Signed-off-by: 郭嘉琪 <guojiaqi@chaomengdata.com>

--- etl/service/forecast.py
+++ etl/service/forecast.py
@@ -72,7 +72,7 @@ class ForecastService:
 
     def lacking_rate(self, cmid, store_id):
         end = datetime.now() - timedelta(days=1)
-        start = end - timedelta(days=30)
+        start = end - timedelta(days=60)
         dates = pd.date_range(start, end, closed="right")
         data = []
         for d in dates:
