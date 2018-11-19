cbd088cb1da8ad6b366b32fe4d48204ecfc94f80
余慕君 <yumujun@chaomengdata.com>
2018-11-19 15:09:40 +0800

    优化抓取客户表结构

--- etl/controllers/admin_api/ext_table.py
+++ etl/controllers/admin_api/ext_table.py
@@ -1,11 +1,9 @@
-from threading import Thread
-
-from flask import current_app
-
-from etl.service.ext_table import ExtTableService
+from etl.service.ext_table import ExtTableService, ExtTaskExists, ExtDatasourceParaMiss, ExtDatasourceConnError
 from . import etl_admin_api, jsonify_with_data, jsonify_with_error
 from .. import APIError
-from flask import request
+from flask import current_app
+from threading import Thread
+service = ExtTableService()
 
 
 @etl_admin_api.route('/tables/download/<source_id>', methods=['GET'])
@@ -14,40 +12,18 @@ def download_tables(source_id):
       "db_name": {"database": "sss","schema": ["schema1", "schema2"]},
         只考虑单数据库，多schema的情况，不考虑多数据库
     """
-    ext_table_service = ExtTableService()
-    # 如果任务已经再，就不重复执行
-    status = ext_table_service.get_status(source_id)
-    if status == 'running':
-        return jsonify_with_data(APIError.PROCESSING, reason="task is running")
-
-    # 测试数据库是否能够正常连接，无法连接就返回错误信息
-    data = ext_table_service.get_datasource_by_source_id(source_id)
-    if data is None:
-        return jsonify_with_error(APIError.NOTFOUND, "Datasource not found")
-
-    db_name = data.get('db_name')
-    if db_name is None:
-        return jsonify_with_error(APIError.BAD_REQUEST, "db_name is missing")
-    database = db_name.get('database')
-    if database is None:
-        return jsonify_with_error(APIError.BAD_REQUEST, "database is missing")
-
-    data['database'] = database
-    error = ext_table_service.connect_test(data)
-    if error:
-        return jsonify_with_error(APIError.BAD_REQUEST, reason=error)
-    task = Thread(target=ext_table_service.download_tables,
-                  args=(current_app._get_current_object(),), kwargs=data)
-
-    task.start()
-
+    try:
+        service.generate_download_table(source_id)
+    except ExtTaskExists as e:
+        return jsonify_with_error(APIError.SERVER_ERROR, reason=str(e))
+    except (ExtDatasourceParaMiss, ExtDatasourceConnError) as e:
+        return jsonify_with_error(APIError.BAD_REQUEST, reason=str(e))
     return jsonify_with_data(APIError.OK)
 
 
 @etl_admin_api.route('/tables/download/status/<source_id>', methods=['GET'])
 def get_download_tables_status(source_id):
-    ext_table_service = ExtTableService()
-    status = ext_table_service.get_status(source_id)
+    status = service.get_status(source_id)
 
     if status:
         return jsonify_with_data(APIError.OK, data={'status': status})
@@ -57,35 +33,16 @@ def get_download_tables_status(source_id):
 
 @etl_admin_api.route('/tables/download/special', methods=['POST'])
 def download_special_table():
-    ext_table_service = ExtTableService()
-
-    request_data = request.get_json()
-    source_id = request_data.get("source_id")
-    schema = request_data.get("schema")
-    tables = request_data.get("tables")
-    if not all([source_id, schema, tables]):
-        return jsonify_with_error(APIError.VALIDATE_ERROR)
-
-    tables = tables.split(",")
-    # 测试数据库是否能够正常连接，无法连接就返回错误信息
-    data = ext_table_service.get_datasource_by_source_id(source_id)
-    if data is None:
-        return jsonify_with_error(APIError.NOTFOUND, "Datasource not found")
-
-    db_name = data.get('db_name')
-    if db_name is None:
-        return jsonify_with_error(APIError.BAD_REQUEST, "db_name is missing")
-    database = db_name.get('database')
-    if database is None:
-        return jsonify_with_error(APIError.BAD_REQUEST, "database is missing")
-
-    data['database'] = database
-    error = ext_table_service.connect_test(data)
-    if error:
-        return jsonify_with_error(APIError.BAD_REQUEST, reason=error)
-
-    ext_table_service.download_special_tables(source_id, schema, tables, data)
-
+    """
+    指定获取某些表的表结构
+    :return:
+    """
+    try:
+        service.download_special_tables()
+    except ExtTaskExists as e:
+        return jsonify_with_error(APIError.SERVER_ERROR, reason=str(e))
+    except (ExtDatasourceParaMiss, ExtDatasourceConnError) as e:
+        return jsonify_with_error(APIError.BAD_REQUEST, reason=str(e))
     return jsonify_with_data(APIError.OK)
 
 
