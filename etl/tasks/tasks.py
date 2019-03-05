74cff30d85c0b330f7db39e48ecb0acdc2808831
liuhuihui <liuhuihui@chaomengdata.com>
2019-03-05 11:46:40 +0800

    添加库存抓数接口

--- etl/tasks/tasks.py
+++ etl/tasks/tasks.py
@@ -1,5 +1,6 @@
 from lambda_fun.load_data.warehouse import Warehouser
 import lambda_fun.extract_data.extract_db_worker as worker
+import lambda_fun.extract_inventory.extract_inv_worker as inv_worker
 from etl.etl import celery
 from etl.service.iqr import IQRService
 from datetime import datetime
@@ -26,15 +27,15 @@ def task_iqr(source_id):
 
 @celery.task(name="etl.task_warehose")
 def task_warehouse(
-    db_url,
-    target_table,
-    data_key,
-    sync_column,
-    date_column,
-    cmid,
-    source_id,
-    warehouse_type,
-    **kwargs,
+        db_url,
+        target_table,
+        data_key,
+        sync_column,
+        date_column,
+        cmid,
+        source_id,
+        warehouse_type,
+        **kwargs,
 ):
     runner = Warehouser(
         db_url, target_table, data_key, sync_column, date_column, cmid, source_id
@@ -53,3 +54,16 @@ def task_extract_data(source_id, query_date, task_type, filename, db_url, **kwar
         db_url=db_url,
     )
     return worker.handler(event, None)
+
+
+@celery.task(name="etl.task_extract_inventory")
+def task_extract_inventory(source_id, query_date, task_type, filename, db_url, **kwargs):
+    """kucun"""
+    event = dict(
+        source_id=source_id,
+        query_date=query_date,
+        task_type=task_type,
+        filename=filename,
+        db_url=db_url,
+    )
+    return inv_worker.handler(event, None)
