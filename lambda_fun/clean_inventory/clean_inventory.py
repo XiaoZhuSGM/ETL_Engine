57ea999f27b6f89df8a9f7593c5c7d7b2b557b96
liuhuihui <liuhuihui@chaomengdata.com>
2019-03-15 15:23:13 +0800

    fix_date

--- lambda_fun/clean_inventory/clean_inventory.py
+++ lambda_fun/clean_inventory/clean_inventory.py
@@ -688,9 +688,10 @@ def handler(event, context):
     origin_table_columns = message["origin_table_columns"]
     converts = message["converts"]
     # inventory_table = list(origin_table_columns.keys())[0]
-    beijing_date = datetime.now(tz=_TZINFO)
+    beijing_date = datetime.now(tz=_TZINFO).strftime('%Y-%m-%d')
     hour = datetime.now(tz=_TZINFO).hour
     print(beijing_date)
+    print(date)
     if beijing_date != date:
         hour = 23
     hour_delta = hour - 3
